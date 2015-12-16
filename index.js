965853dc727921ed7af1a20c798468ebf2c6b0ba
Jonathan Gertig <jcgertig@gmail.com>
2015-12-16 14:31:30 -0500

    missing comma

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var React = require('react-native');
 
 module.exports = {
   Router: Router,
-  IndexRoute: require('./src/IndexRoute')
+  IndexRoute: require('./src/IndexRoute'),
   Route: require('./src/Route'),
   Link: require('./src/Link'),
   Transitions: React.Navigator.SceneConfigs,
