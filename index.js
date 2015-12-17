7db0e77bba37fd08d0ae476d25cc875d80c42ab6
Jonathan Gertig <jcgertig@gmail.com>
2015-12-16 22:20:19 -0500

    require router

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var React = require('react-native');
 
 module.exports = {
-  Router: Router,
+  Router: require('./src/Router'),
   IndexRoute: require('./src/IndexRoute'),
   Route: require('./src/Route'),
   Link: require('./src/Link'),
