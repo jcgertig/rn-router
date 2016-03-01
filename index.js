1464fa412644c54b84ff746a225166c604279a11
Jonathan Gertig <jcgertig@gmail.com>
2016-03-01 10:27:27 -0500

    add none and fade transitons and up version to 2.0.0

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var React = require('react-native');
 
-let Transitions = Object.assign({}, React.Navigator.SceneConfigs, { None: 'NO_TRANSITION' })
 module.exports = {
   Router: require('./src/Router'),
   IndexRoute: require('./src/IndexRoute'),
   Route: require('./src/Route'),
   Link: require('./src/Link'),
-  Transitions: Transitions,
+  Transitions: require('./src/Transitions'),
 };
